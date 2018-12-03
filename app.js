7a0674057cf5e53103fb835324f8ef10f044c780
Andrew DaRe <andrew@duh.re>
2018-12-02 21:18:47 -0800

    Added sample response message

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 app.get('/', (req, res) => {
   res.json({'test': 'payload'});
+});
+
+app.post('/google', jsonParser, (req, res) => {
+  console.log('request hit');
+  // console.log(req.body.queryResult.action);
+  res.json({
+    "payload": {
+      "google": {
+        "expectUserResponse": false,
+        "richResponse": {
+          "items": [
+            {
+              "simpleResponse": {
+                "textToSpeech": "hey dude"
+              }
+            }
+          ]
+        }
+      }
+    }
+  })
 });
 
 app.listen(3000, () => {})
