c929e8f5b06d51d8054f332f029e31ce0d3b490f
Andrew DaRe <andrew@duh.re>
2018-12-02 22:43:26 -0800

    Changed welcome text

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.post('/google', jsonParser, (req, res) => {
           "items": [
             {
               "simpleResponse": {
-                "textToSpeech": "hey dude",
-                "displayText": "Hello there! How's it hanging"
+                "textToSpeech": "Welcome to Repeated. Tell us what you'd like to repeat, followed by the number of times.",
+                "displayText": "Welcome to Repeated. Tell us what you'd like to repeat, followed by the number of times."
               }
             }
           ]
